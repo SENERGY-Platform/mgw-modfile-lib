62813f11933fc22113652f208b63e76ecbaf7a12
y-du <42994541+y-du@users.noreply.github.com>
2023-03-15 16:39:08 +0100

    add GenTmpfsMounts test

--- v1/generator/services/generator_test.go
+++ v1/generator/services/generator_test.go
@@ -96,6 +96,7 @@ func TestGenBindMounts(t *testing.T) {
 	} else if len(bm) != 1 {
 		t.Errorf("len(%v) != 1", bm)
 	}
+	mfBMs = mfBMs[:len(mfBMs)-1]
 	// --------------------------------
 	mfBMs = append(mfBMs, model.BindMount{
 		MountPoint: str,
@@ -105,8 +106,8 @@ func TestGenBindMounts(t *testing.T) {
 	if _, err := GenBindMounts(mfBMs); err == nil {
 		t.Error("err == nil")
 	}
-	// --------------------------------
 	mfBMs = mfBMs[:len(mfBMs)-1]
+	// --------------------------------
 	mfBMs = append(mfBMs, model.BindMount{
 		MountPoint: str,
 		Source:     str2,
@@ -114,5 +115,101 @@ func TestGenBindMounts(t *testing.T) {
 	})
 	if _, err := GenBindMounts(mfBMs); err == nil {
 		t.Error("err == nil")
+	}
+}
+
+func TestGenTmpfsMounts(t *testing.T) {
+	var mfTMs []model.TmpfsMount
+	if tm, err := GenTmpfsMounts(mfTMs); err != nil {
+		t.Error("err != nil")
+	} else if len(tm) != 0 {
+		t.Errorf("len(%v) != 0", tm)
+	}
+	// --------------------------------
+	str := "test"
+	mfTMs = append(mfTMs, model.TmpfsMount{
+		MountPoint: str,
+		Size:       64,
+		Mode:       nil,
+	})
+	a := module.TmpfsMount{
+		Size: 64,
+		Mode: 504,
+	}
+	if tm, err := GenTmpfsMounts(mfTMs); err != nil {
+		t.Error("err != nil")
+	} else if len(tm) != 1 {
+		t.Errorf("len(%v) != 1", tm)
+	} else if b, ok := tm[str]; !ok {
+		t.Errorf("m, ok := tm[%s]; !ok", str)
+	} else if reflect.DeepEqual(a, b) == false {
+		t.Errorf("%+v != %+v", a, b)
+	}
+	// --------------------------------
+	mfTMs = append(mfTMs, model.TmpfsMount{
+		MountPoint: str,
+		Size:       64,
+		Mode:       nil,
+	})
+	if tm, err := GenTmpfsMounts(mfTMs); err != nil {
+		t.Error("err != nil")
+	} else if len(tm) != 1 {
+		t.Errorf("len(%v) != 1", tm)
+	}
+	mfTMs = mfTMs[:len(mfTMs)-1]
+	// --------------------------------
+	fm := model.FileMode(504)
+	mfTMs = append(mfTMs, model.TmpfsMount{
+		MountPoint: str,
+		Size:       64,
+		Mode:       &fm,
+	})
+	if tm, err := GenTmpfsMounts(mfTMs); err != nil {
+		t.Error("err != nil")
+	} else if len(tm) != 1 {
+		t.Errorf("len(%v) != 1", tm)
+	}
+	mfTMs = mfTMs[:len(mfTMs)-1]
+	// --------------------------------
+	mfTMs = append(mfTMs, model.TmpfsMount{
+		MountPoint: str,
+		Size:       32,
+		Mode:       nil,
+	})
+	if _, err := GenTmpfsMounts(mfTMs); err == nil {
+		t.Error("err == nil")
+	}
+	mfTMs = mfTMs[:len(mfTMs)-1]
+	// --------------------------------
+	fm = model.FileMode(511)
+	mfTMs = append(mfTMs, model.TmpfsMount{
+		MountPoint: str,
+		Size:       64,
+		Mode:       &fm,
+	})
+	if _, err := GenTmpfsMounts(mfTMs); err == nil {
+		t.Error("err == nil")
+	}
+	mfTMs = mfTMs[:len(mfTMs)-1]
+	// --------------------------------
+	str2 := "test2"
+	fm2 := model.FileMode(511)
+	mfTMs = append(mfTMs, model.TmpfsMount{
+		MountPoint: str2,
+		Size:       64,
+		Mode:       &fm2,
+	})
+	a = module.TmpfsMount{
+		Size: 64,
+		Mode: 511,
+	}
+	if tm, err := GenTmpfsMounts(mfTMs); err != nil {
+		t.Error("err != nil")
+	} else if len(tm) != 2 {
+		t.Errorf("len(%v) != 2", tm)
+	} else if b, ok := tm[str2]; !ok {
+		t.Errorf("m, ok := tm[%s]; !ok", str2)
+	} else if reflect.DeepEqual(a, b) == false {
+		t.Errorf("%+v != %+v", a, b)
 	}
 }
