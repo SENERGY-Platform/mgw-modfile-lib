e039779dc1ed9fcf0e79d54b1a57a03c72746636
y-du <42994541+y-du@users.noreply.github.com>
2023-03-16 18:13:17 +0100

    add ByteFmt UnmarshalYAML test

---
+++ v1/model/util_test.go
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package model
+
+import (
+	"gopkg.in/yaml.v3"
+	"testing"
+)
+
+func TestByteFmt_UnmarshalYAML(t *testing.T) {
+	a := ByteFmt(67108864)
+	var b ByteFmt
+	if err := yaml.Unmarshal([]byte("64Mb"), &b); err != nil {
+		t.Error("err != nil")
+	} else if a != b {
+		t.Errorf("%d != %d", a, b)
+	}
+	// ---------------------------
+	if err := yaml.Unmarshal([]byte("67108864"), &b); err != nil {
+		t.Error("err != nil")
+	} else if a != b {
+		t.Errorf("%d != %d", a, b)
+	}
+	// ---------------------------
+	if err := yaml.Unmarshal([]byte("test"), &b); err == nil {
+		t.Error("err == nil")
+	}
+	// ---------------------------
+	if err := yaml.Unmarshal([]byte("1.1"), &b); err == nil {
+		t.Error("err == nil")
+	}
+}
